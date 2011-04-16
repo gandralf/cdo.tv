f0346c855c587ec8d3bf3dc8111069c52ca15905
Eurico Vidal <euricovidal@hotmail.com>
2011-04-16 16:15:50 -0300

    Ajustando o footer e adicionado arquivo de functions

--- wp-content/themes/cdotv/footer.php
+++ wp-content/themes/cdotv/footer.php
@@ -1,38 +1,26 @@
-<script src="<?php bloginfo( 'template_url' ); ?>/js/AC_RunActiveContent.js" type="text/javascript"></script>
-<!--			footer -->
-<div id="footer">
-	<div class="content">
-		<div class="fNav">
-			<h5>Entretención</h5>
-			<ul>
-				<li><a href="programacion.html" target='_self'>Programación</a></li>
-				<li><a href="contenido.html" target='_self'>Contenido</a></li>
-				<li><a href="multimedia.html" target='_self'>Multimedia</a></li>
-			</ul>
-		</div>
-		<div class="fNav">
-			<h5>CDO por dentro</h5>
-			<ul>
-				<li><a href="quienessomos.html" target='_self'>¿Quienes somos?</a></li>
-				<li><a href="mision.html" target='_self'>Misión</a></li>
-				<li><a href="equipo.html"target='_self'>Equipo</a></li>
-			  <li><a href="contacto.html" target='_self'>Contacto</a></li>
-			</ul>
-	  </div>
-		<div class="cdo">
-			<img src="images/logoFooter.gif" /><br/>
-			<p>600.600.1000<br/>
-			Av. Vicuña Maquena, 1000<br/>
-			Santiago - Chile</p>
-			<p class="powered">Powered by <br/><a href="http://www.gonow.com.br" target='_blank'><img src="images/logoGonow.gif" /></a></p>
-			
-		</div>
-		<p class="copyright">c 2010 CDO Todos los derechos reservados</p>
-	</div>
+    <div id="footer">
+        <div class="content">
+            <?php get_sidebar('footer');?>
+            <div class="fNav">
+                <h5>CDO por dentro</h5>
+                <ul>
+                    <li><a href="quienessomos.html" target='_self'>¿Quienes somos?</a></li>
+                    <li><a href="mision.html" target='_self'>Misión</a></li>
+                    <li><a href="equipo.html"target='_self'>Equipo</a></li>
+                    <li><a href="contacto.html" target='_self'>Contacto</a></li>
+                </ul>
+            </div>
+            <div class="cdo">
+                <img src="images/logoFooter.gif" /><br/>
+                <p>600.600.1000<br/>
+                Av. Vicuña Maquena, 1000<br/>
+                Santiago - Chile</p>
+                <p class="powered">Powered by <br/><a href="http://www.gonow.com.br" target='_blank'><img src="images/logoGonow.gif" /></a></p>
+            </div>
+            <p class="copyright">c 2010 CDO Todos los derechos reservados</p>
+        </div>
+    </div>
 </div>
-
-<!-- 			footer -->
-
-
+<script src="<?php bloginfo( 'template_url' ); ?>/js/AC_RunActiveContent.js" type="text/javascript"></script>
 </body>
 </html>
---
+++ wp-content/themes/cdotv/functions.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * TwentyTen functions and definitions
+ *
+ * @package WordPress
+ * @subpackage CDO.TV
+ * @since CDO.TV 1.0
+ */
+
+function cdotv_widgets_init() {
+	register_sidebar( array(
+		'name' => __( 'Primary Widget Area', 'twentyten' ),
+		'id' => 'primary-widget-area',
+		'description' => __( 'The primary widget area', 'twentyten' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Secondary Widget Area', 'twentyten' ),
+		'id' => 'secondary-widget-area',
+		'description' => __( 'The secondary widget area', 'twentyten' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'First Footer Widget Area', 'twentyten' ),
+		'id' => 'first-footer-widget-area',
+		'description' => __( 'The first footer widget area', 'twentyten' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Second Footer Widget Area', 'twentyten' ),
+		'id' => 'second-footer-widget-area',
+		'description' => __( 'The second footer widget area', 'twentyten' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Third Footer Widget Area', 'twentyten' ),
+		'id' => 'third-footer-widget-area',
+		'description' => __( 'The third footer widget area', 'twentyten' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+	register_sidebar( array(
+		'name' => __( 'Fourth Footer Widget Area', 'twentyten' ),
+		'id' => 'fourth-footer-widget-area',
+		'description' => __( 'The fourth footer widget area', 'twentyten' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'cdotv_widgets_init' );
