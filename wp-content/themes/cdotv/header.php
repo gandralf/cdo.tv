4784c373e87915cbb696a61ef8c77a8f8d725e86
rafael <rafael.madureira.santos@gmail.com>
2011-04-16 15:28:40 -0300

    Criando header do site

---
+++ wp-content/themes/cdotv/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title><?php wp_title(); ?></title>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+</head>
+
+<body>
+
+<div id="tudo">
+  <div id="top">
+    <img src="<?php echo bloginfo('template_url');?>/images/logo.png"/>
+	<div id="banner">
+		<img src="<?php echo bloginfo('template_url');?>/images/banner.gif" />
+	</div>
+  </div>
+  
+  <div id="conteudo">
+    <div id="menu"><div align="center"><img src="<?php echo bloginfo('template_url');?>/images/menuhome.png" width="926" height="35" border="0" usemap="#Map" />
--- wp-content/themes/cdotv/index.php
+++ wp-content/themes/cdotv/index.php
@@ -1,28 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>CDO - Canal Deporte Olimpico</title>
-<link href="style.css" rel="stylesheet" type="text/css" />
-<style type="text/css">
-<!--
+<?php get_header();?>
 
--->
-</style>
-<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
-</head>
-<body>
-
-<div id="tudo">
-  <div id="top">
-    <img src="images/logo.png"/>
-	<div id="banner">
-		<img src="images/banner.gif" />
-	</div>
-  </div>
-  
-  <div id="conteudo">
-    <div id="menu"><div align="center"><img src="images/menuhome.png" width="926" height="35" border="0" usemap="#Map" />
 <map name="Map" id="Map">
   <area shape="rect" coords="11,5,184,26" href="index.html" target="_self" />
   <area shape="rect" coords="731,4,904,25" href="multimedia.html" target="_self" />
