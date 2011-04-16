74f144a8fc188857a5fc14ccaa9fee68ce24302b
rafael <rafael.madureira.santos@gmail.com>
2011-04-16 17:00:15 -0300

    Exibição do logo da gonow

--- wp-content/themes/cdotv/footer.php
+++ wp-content/themes/cdotv/footer.php
@@ -6,7 +6,7 @@
         <?php dynamic_sidebar('third-footer-widget-area');?>
         <?php dynamic_sidebar('fourth-footer-widget-area');?>
         <p class="copyright powered">
-            Powered by <br/><a href="http://www.gonow.com.br" target='_blank'><img src="images/logoGonow.gif" /></a></p>
+            Powered by <br/><a href="http://www.gonow.com.br" target='_blank'><img src="<?php echo bloginfo('template_url');?>/images/logoGonow.gif" /></a></p>
         <p class="copyright">c 2010 CDO Todos los derechos reservados</p>
     </div>
 </div>
