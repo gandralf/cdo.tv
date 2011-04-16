6eb1d5516c5b6a943781269ce9ac0f260af30c73
Eurico Vidal <euricovidal@hotmail.com>
2011-04-16 15:26:54 -0300

    Separando footer da home - DUMP

--- wp-content/themes/cdotv/index.php
+++ wp-content/themes/cdotv/index.php
@@ -9,7 +9,6 @@
 
 -->
 </style>
-<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
 </head>
 <body>
 
@@ -55,41 +54,4 @@ AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/
     </div>
   </div>
 </div>
-
-<!--			footer -->
-<div id="footer">
-	<div class="content">
-		<div class="fNav">
-			<h5>Entretención</h5>
-			<ul>
-				<li><a href="programacion.html" target='_self'>Programación</a></li>
-				<li><a href="contenido.html" target='_self'>Contenido</a></li>
-				<li><a href="multimedia.html" target='_self'>Multimedia</a></li>
-			</ul>
-		</div>
-		<div class="fNav">
-			<h5>CDO por dentro</h5>
-			<ul>
-				<li><a href="quienessomos.html" target='_self'>¿Quienes somos?</a></li>
-				<li><a href="mision.html" target='_self'>Misión</a></li>
-				<li><a href="equipo.html"target='_self'>Equipo</a></li>
-			  <li><a href="contacto.html" target='_self'>Contacto</a></li>
-			</ul>
-	  </div>
-		<div class="cdo">
-			<img src="images/logoFooter.gif" /><br/>
-			<p>600.600.1000<br/>
-			Av. Vicuña Maquena, 1000<br/>
-			Santiago - Chile</p>
-			<p class="powered">Powered by <br/><a href="http://www.gonow.com.br" target='_blank'><img src="images/logoGonow.gif" /></a></p>
-			
-		</div>
-		<p class="copyright">c 2010 CDO Todos los derechos reservados</p>
-	</div>
-</div>
-
-<!-- 			footer -->
-
-
-</body>
-</html>
+<?php get_footer();?>
