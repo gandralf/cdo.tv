4fa07a61f741a4c2f855956fbc7fba9c73f4707b
rafael <rafael.madureira.santos@gmail.com>
2011-04-17 22:07:21 -0300

    Atualização de controle de texto nos box's de destaque da página home

--- wp-content/themes/cdotv/highlight.php
+++ wp-content/themes/cdotv/highlight.php
@@ -16,24 +16,32 @@
 				echo '<ul class="highlight-list-home">';
 				$count = 0;
 				while(have_posts()){
-					
 					the_post();
-						if (($count%2)==0) {
-							echo '<li class="highlight-list-item-off-home">';
-						} else {
-							echo '<li class="highlight-list-item-on-home">';
-						}
-						echo '<a href="'.get_permalink().'" title="'.the_title_attribute('echo=0').'" class="new_content">';
-						if (class_exists('MultiPostThumbnails') && MultiPostThumbnails::has_post_thumbnail('contenido', 'second-featured-image')) {
-							MultiPostThumbnails::the_post_thumbnail('contenido', 'second-featured-image', NULL, 'second-featured-image-thumbnail'); 
-						}
-						echo '<h3>';
-						$title = get_the_title();
-						echo splitText($title, 30);
-						echo '</h3>';
-						echo '<p>'. splitText(get_the_excerpt(), 80) .'</p>';
-						echo '</a>';
-						echo '</li>';
+					
+					if (($count%2)==0) {
+						echo '<li class="highlight-list-item-off-home">';
+					} else {
+						echo '<li class="highlight-list-item-on-home">';
+					}
+					
+					echo '<a href="'.get_permalink().'" title="'.the_title_attribute('echo=0').'" class="new_content">';
+					
+					if (class_exists('MultiPostThumbnails') && MultiPostThumbnails::has_post_thumbnail('contenido', 'second-featured-image')) {
+						MultiPostThumbnails::the_post_thumbnail('contenido', 'second-featured-image', NULL, 'second-featured-image-thumbnail'); 
+					}
+					
+					$video = get_post_meta($post->ID, 'video', true);
+					if ($video != '') {
+						echo '<img class="icone-video" src="'. get_bloginfo('template_url') . '/images/icon-video.png" />';
+					}
+					
+					echo '<h3>';
+					$title = get_the_title();
+					echo nl2br(splitText(wordwrap($title, 30, "\n"), 35));
+					echo '</h3>';
+					echo '<p>'. splitText(get_the_excerpt(), 80) .'</p>';
+					echo '</a>';
+					echo '</li>';
 					$count++;
 				}
 				echo '</ul>';
