2b11f28f9d51d967859d70fcc2a923cdfd489de9
rodrigomilfont <rodrigomilfont@gmail.com>
2011-04-19 13:42:48 -0300

    programacion grade

--- wp-content/themes/cdotv/programacion.php
+++ wp-content/themes/cdotv/programacion.php
@@ -9,4 +9,47 @@
 
 <?php get_header();?>
 
+<div id="programacion">
+
+    <div class="box_header_programacion">
+    	<h2 class="dia_actual">9 Abr</h2>
+        <div class="box_grade_programacion">
+            <span class="bt_anterior"><a href="#">Alterior</a></span>
+            <ul>
+            	<li>
+            		<div class="box_hora_esq">
+            			<h3>18:00 pm</h3>
+            			<span>18:00 pm</span>
+            			<span>18:15 pm</span>
+            			<span>18:30 pm</span>
+            			<span class="last">18:45 pm</span>
+            		</div>
+                        <span class="bottom">&nbsp;</span>
+            	</li>
+            	<li class="central">
+            		<div class="box_hora_central">
+            			<h3>18:00 pm</h3>
+            			<span>18:00 pm</span>
+            			<span>18:15 pm</span>
+            			<span>18:30 pm</span>
+            			<span class="last">18:45 pm</span>
+            		</div>
+                        <span class="bottom">&nbsp;</span>
+            	</li>
+            	<li>
+            		<div class="box_hora_dir">
+            			<h3>18:00 pm</h3>
+            			<span>18:00 pm</span>
+            			<span>18:15 pm</span>
+            			<span>18:30 pm</span>
+            			<span class="last">18:45 pm</span>
+            		</div>
+                        <span class="bottom">&nbsp;</span>
+            	</li>
+            </ul>
+            <span class="bt_proxima"><a href="#">Proxima</a></span>
+        </div>
+    </div>
+
+</div>
 <?php get_footer();?>
