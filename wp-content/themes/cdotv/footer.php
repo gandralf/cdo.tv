efe99f85a138ab2ac5d0d27d226065939d92f2c4
rodrigomilfont <rodrigomilfont@gmail.com>
2011-04-19 17:18:30 -0300

    alteracoes estruturais

--- wp-content/themes/cdotv/footer.php
+++ wp-content/themes/cdotv/footer.php
@@ -1,13 +1,16 @@
+    </div>
+        <!-- /CONTENT -->
 </div>
+        <!-- /CONTAINER -->
 <div id="footer">
-    <div class="content">
+    <div class="content_footer">
         <?php dynamic_sidebar('first-footer-widget-area');?>
         <?php dynamic_sidebar('second-footer-widget-area');?>
         <?php dynamic_sidebar('third-footer-widget-area');?>
         <?php dynamic_sidebar('fourth-footer-widget-area');?>
         <p class="powered">
             Powered by <br/><a href="http://www.gonow.com.br" target='_blank'><img src="<?php echo bloginfo('template_url');?>/images/logoGonow.gif" /></a></p>
-        <p class="copyright">c 2010 CDO Todos los derechos reservados</p>
+        <p class="copyright">&copy; 2010 CDO Todos los derechos reservados</p>
     </div>
     
 </div>
--- wp-content/themes/cdotv/header.php
+++ wp-content/themes/cdotv/header.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title><?php
-global $page, $paged;
-wp_title('|', true, 'right');
-bloginfo('name');
-$site_description = get_bloginfo('description', 'display');
-if($site_description && (is_home() || is_front_page()))
-    echo " | $site_description";
-if($paged >= 2 || $page >= 2)
-    echo ' | ' . sprintf( 'Page %s', max( $paged, $page ));
-?></title>
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-</head>
-<body>
-<div id="tudo">
-	<div id="top">
-		<img src="<?php echo bloginfo('template_url');?>/images/logo.png"/>
-		<div id="banner">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title><?php
+        global $page, $paged;
+        wp_title('|', true, 'right');
+        bloginfo('name');
+        $site_description = get_bloginfo('description', 'display');
+        if($site_description && (is_home() || is_front_page()))
+            echo " | $site_description";
+        if($paged >= 2 || $page >= 2)
+            echo ' | ' . sprintf( 'Page %s', max( $paged, $page ));
+        ?></title>
+            <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    </head>
+    <body>
+        <!-- CONTAINER -->
+        <div id="container">
+            <!-- HEADER -->
+            <div id="header">
+                    <h1 class="logo">CDO - CANAL DEPORTE OLIMPICO</h1>
+                    <!-- BANNER -->
+                    <div class="banner">
 <?php
 $args = array('post_type' => 'banner',
     'post_status' => 'publish',
@@ -26,23 +29,24 @@ $args = array('post_type' => 'banner',
     'posts_per_page' => 1);
 $wp_query = new WP_Query($args);
 if(have_posts()): while(have_posts()): the_post();
-    if(class_exists('MultiPostThumbnails') && MultiPostThumbnails::has_post_thumbnail('banner', 'banner-image')):
-        echo '<a href="'.validateUrl(get_the_title()).'" target="_blank">';
-        MultiPostThumbnails::the_post_thumbnail('banner', 'banner-image', null, 'banner-image-thumbnail');
-        echo '</a>';
-    endif;
+if(class_exists('MultiPostThumbnails') && MultiPostThumbnails::has_post_thumbnail('banner', 'banner-image')):
+    echo '<a href="'.validateUrl(get_the_title()).'" target="_blank">';
+MultiPostThumbnails::the_post_thumbnail('banner', 'banner-image', null, 'banner-image-thumbnail');
+echo '</a>';
+endif;
 endwhile;endif;wp_reset_query();
 ?>
-		</div>
-	</div>
-	<div id="conteudo">
-		<center>
-			<div id="menu">
-				<div class="menu-border-left"></div>
-				<div class="menu-center">
-					<?php wp_nav_menu( array('menu' => 'Menu header'));?>
-				</div>
-				<div class="menu-border-right"></div>
-			</div>
-		</center>
-	</div>
+                </div>
+                <!-- /BANNER -->
+                <div id="menu">
+                        <div class="menu-border-left"></div>
+                        <div class="menu-center">
+                                <?php wp_nav_menu( array('menu' => 'Menu header'));?>
+                        </div>
+                        <div class="menu-border-right"></div>
+                </div>
+        </div>
+        <!-- /HEADER -->
+        <!-- CONTENT -->
+        <div class="content">
+
